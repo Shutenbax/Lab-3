e29f9eb9e26eedce4c2aa2d7443f75550b921bca
Shutenbax <paladins.21.12@mail.ru>
2020-10-22 14:43:51 +0300

    asd

--- lab-4/lab-4_krivosheev.lua
+++ lab-4/lab-4_krivosheev.lua
@@ -15,11 +15,11 @@ ui = bld.objects
 function ui.But_Add:on_clicked(...)
 	name = ui.txt_name.text
 	value = tonumber(ui.txt_value.text)
-
-	px = pixbuf.new_from_file(ui.fileref[ui.Combo])
+	sale = tonumber(ui.txt_sale.text)
+	px = pixbuf.new_from_file(ui.Fruit_txt.text)
 
 	i = ui.mdl_items:append()
-	ui.mdl_items[i] = {[1] = name, [2] = value, [3] = px}
+	ui.mdl_items[i] = {[1] = name, [2] = value, [3] = px, [4]=sale}
 end
 
 rdr_txt = gtk.CellRendererText {}
@@ -28,10 +28,20 @@ rdr_pix = gtk.CellRendererPixbuf {}
 c1 = gtk.TreeViewColumn { title = 'Name', {rdr_txt, { text = 1 }}}
 c2 = gtk.TreeViewColumn { title = 'Value', {rdr_txt, { text = 2 }}}
 c3 = gtk.TreeViewColumn { title = 'Image', {rdr_pix, { pixbuf = 3 }}}
+c4 = gtk.TreeViewColumn { title = 'Sale', {rdr_txt, { text = 4 }}}
 
 ui.lst_items:append_column(c1)
 ui.lst_items:append_column(c2)
 ui.lst_items:append_column(c3)
+ui.lst_items:append_column(c4)
+
+function ui.Instal:on_clicked(...)
+	file = io.open("PoleInput","r+")
+	for y=0, 2, 1 do
+	io.write(ui.mdl_items[y])
+	end
+	io.close(file)
+end
 
 ui.wnd.title = 'lab'
 ui.wnd.on_destroy = gtk.main_quit
