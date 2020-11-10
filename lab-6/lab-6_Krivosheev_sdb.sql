9b978279054fedbf78d9a10bb6b296440d7c31f7
Shutenbax <paladins.21.12@mail.ru>
2020-11-10 18:12:32 +0300

    do

---
+++ lab-6/lab-6_Krivosheev_sdb.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (id INT, name TEXT);
+
+INSERT INTO employee VALUES (1, 'Alexey Krivosheev');
+INSERT INTO employee VALUES (2, 'Dezmund');
+INSERT INTO employee VALUES (3, 'Erdis');
+INSERT INTO employee VALUES (4, 'Torvald');
+INSERT INTO employee VALUES (5, 'Deimos');
+
+
+DROP TABLE IF EXISTS mapping;
+CREATE TABLE mapping (idSlave INT, idLanguage INT);
+
+INSERT INTO language VALUES (1, 1);
+INSERT INTO language VALUES (1, 2);
+INSERT INTO language VALUES (1, 3);
+INSERT INTO language VALUES (1, 4);
+
+INSERT INTO language VALUES (2, 2);
+INSERT INTO language VALUES (2, 3);
+
+INSERT INTO language VALUES (3, 1);
+INSERT INTO language VALUES (3, 4);
+INSERT INTO language VALUES (3, 5);
+
+INSERT INTO language VALUES (4, 5);
+INSERT INTO language VALUES (4, 6);
+
+INSERT INTO language VALUES (1, 1);
+INSERT INTO language VALUES (1, 2);
+INSERT INTO language VALUES (1, 3);
+INSERT INTO language VALUES (1, 4);
+INSERT INTO language VALUES (1, 5);
+
+
+DROP TABLE IF EXISTS language;
+CREATE TABLE language (id INT, nameLanguage TEXT);
+
+INSERT INTO language VALUES (1, 'c#');
+INSERT INTO language VALUES (2, 'pascal');
+INSERT INTO language VALUES (3, 'javascript');
+INSERT INTO language VALUES (4, 'lua');
+INSERT INTO language VALUES (5, 'html');
+INSERT INTO language VALUES (6, '1c');
+
+
+
--- lab-6/lab-6_krivosheev.lua
+++ lab-6/lab-6_krivosheev.lua
@@ -12,31 +12,48 @@ bld = gtk.Builder()
 bld:add_from_file('lab-6_krivosheev.glade')
 
 ui = bld.objects
+ui.wnd.title = 'lab-6_krivosheev'
 
 rdr_txt = gtk.CellRendererText {}
 rdr_pix = gtk.CellRendererPixbuf {}
 
 c1 = gtk.TreeViewColumn { title = 'Name', {rdr_txt, { text = 1 }}}
-c2 = gtk.TreeViewColumn { title = 'Value', {rdr_txt, { text = 2 }}}
+c2 = gtk.TreeViewColumn { title = 'Volue', {rdr_txt, { text = 2 }}}
 c3 = gtk.TreeViewColumn { title = 'Image', {rdr_pix, { pixbuf = 3 }}}
-c4 = gtk.TreeViewColumn { title = 'Sale', {rdr_txt, { text = 1 }}}
+c4 = gtk.TreeViewColumn { title = 'Sale', {rdr_txt, { text = 4 }}}
 
 ui.lst_items:append_column(c1)
 ui.lst_items:append_column(c2)
 ui.lst_items:append_column(c3)
 ui.lst_items:append_column(c4)
 
+
 db = sqlite.open('lab-6_krivosheev.db')
 
-for row in db:rows('SELECT * FROM list') do
+for row in db:nrows('SELECT * FROM list') do
 	px = pixbuf.new_from_file(row.image)
-
+	print(row.name)
 	el = ui.stor_items:append()
-	ui.stor_items[el] = { [1] = row.name, [2] = row.value, [3] = px, [4] = row.sale }
+	ui.stor_items[el] = { [1] = row.name, [2] = row.volue, [3] = px, [4] = row.sale }
 end
 
 db:close()
 
+--[[
+local base = 'lab-04'
+local sql = 'SELECT * FROM list'
+f = io.popen('sqlite3 lab-6_krivosheev.db "lab-6_krivosheev.sql"')
+while true do
+	local row = f:read('*l')
+	print(row)
+	if row == nil then break end
+	local _, _, name, volue, image, sale = row:find('(%w+)|(%d+)|(.-)|(%d+)$')
+	px = pixbuf.new_from_file(image)
+	el = ui.stor_items:append()
+	ui.stor_items[el] = { [1] = row.name, [2] = row.volue, [3] = px, [4] = row.sale }
+end
+f:close()
+]]
 ui.wnd.on_destroy = gtk.main_quit
 ui.wnd:show_all()
 
